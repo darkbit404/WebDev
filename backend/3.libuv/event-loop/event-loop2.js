33b4a1706459a47c0aa6d2ca6eb2597a1a94158d
Pratyush Kumar Singh <pratyushksingh2001@gmail.com>
2025-03-20 18:19:15 +0530

    libuv

---
+++ backend/3.libuv/event-loop/event-loop2.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+
+const a = 100;
+
+setImmediate(() => console.log("SetImmediate"));
+
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", () => {
+    console.log("File Reading Complete!");
+});
+
+setTimeout(() => console.log("Timer Expired!"), 0);
+
+process.nextTick(() => console.log("process.nextTick"));
+
+function printA()
+{
+    console.log("a= ", a);
+}
+
+printA();
+console.log("Last line of the file.");
+
+//While executiion if the event loop becomes idle.
