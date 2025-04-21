6bfd87c4dd1e21304b9baacd24c3716ea2f8e623
Pratyush Kumar Singh <pratyushksingh2001@gmail.com>
2025-04-21 05:44:44 +0530

    express server

--- devconnect/backend/src/app.js
+++ devconnect/backend/src/app.js
@@ -5,8 +5,27 @@ const express = require("express");
 const app = express(); 
 
 //Handling the requests
+//app.use handles all the HTTP method API calls
+app.use("/Home", (req, res) => {
+    res.send("Hello. This is home route.")
+});
+
+app.use("/", (req, res) => {
+    res.send("Hello. This is / route.")
+});
+
 app.use("/test", (req, res) => {
-    res.send("Hello.")
+    res.send("Hello. This is test route.")
+});
+
+//This will handle only the GET call
+app.get("/user", (req, res) => {
+    res.send("Hello. This is user GET route.")
+});
+
+//This will handle only the POST call
+app.post("/user", (req, res) => {
+    res.send("Hello. This is user GET route.")
 });
 
 //We call listen() to listen on some port so that users can connect to the server.
